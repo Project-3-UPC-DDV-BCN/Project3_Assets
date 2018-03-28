3d901c00e3142f049a2e2f68956618eaab03fe36
GuillemD <gdrdvilla@gmail.com>
2018-03-28 15:56:48 +0200

    Tiefighter and xwing prefabs

---
+++ Assets/Scripts/Ai_OA.cs
@@ -0,0 +1,40 @@
+using TheEngine;
+using TheEngine.TheConsole;
+
+public class Ai_OA {
+
+	public TheGameObject parent;
+	TheTransform parent_transform;
+	TheTransform transform;
+
+	public float Mnv = 10.0f;
+
+	void Start () {
+		transform = TheGameObject.Self.GetComponent<TheTransform>();
+		if(parent == null)
+			parent = TheGameObject.Self.GetParent();
+		if(parent != null)
+			parent_transform = parent.GetComponent<TheTransform>();
+	}
+	
+	void Update () {
+		if(parent == null)
+			return;
+		transform.LocalPosition = parent_transform.ForwardDirection.Normalized;
+		
+	}
+
+	void OnTriggerStay(TheGameObject other) {
+		if(other == parent) 
+			return;
+		if(other == null || parent == null)
+			return;
+		TheVector3 colDir = other.GetComponent<TheTransform>().GlobalPosition - parent_transform.GlobalPosition;
+		TheVector3 newRot = TheVector3.Reflect(-colDir, parent_transform.ForwardDirection);
+		float aux = newRot.z;
+		newRot.z = newRot.x;
+		newRot.x = aux;
+		parent_transform.GlobalRotation += newRot.Normalized * Mnv * TheTime.DeltaTime;
+		
+	}
+}
